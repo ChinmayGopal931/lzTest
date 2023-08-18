0960311ee4a946dfae98ebd14f57af2266aa944b
ChinmayGopal931 <chinmayg015@gmail.com>
2023-08-17 23:50:50 -0700

    add dfx contracts

--- test/contracts/oft/v2/ComposableProxyOFTV2.test.js
+++ test/contracts/oft/v2/ComposableProxyOFTV2.test.js
@@ -8,6 +8,7 @@ describe("Composable ProxyOFT v2: ", function () {
     let srcEndpoint, dstEndpoint, proxyOFT, dstOFT, srcStaking, dstStaking, dstPath, srcPath, token
     let owner, alice, bob, carol
     let dstStakingAddressBytes32, srcStakingAddressBytes32
+    let cert
 
     before(async function () {
         const LZEndpointMock = await ethers.getContractFactory("LZEndpointMock")
@@ -15,41 +16,49 @@ describe("Composable ProxyOFT v2: ", function () {
         const MockToken = await ethers.getContractFactory("MockToken")
         const OFT = await ethers.getContractFactory("OFTV2")
         const OFTStakingMock = await ethers.getContractFactory("OFTStakingMockV2")
-
         srcEndpoint = await LZEndpointMock.deploy(srcChainId)
         dstEndpoint = await LZEndpointMock.deploy(dstChainId)
         token = await MockToken.deploy("Mock", "MOCK")
-
         proxyOFT = await ProxyOFT.deploy(token.address, 6, srcEndpoint.address)
         dstOFT = await OFT.deploy("OFT", "OFT", 6, dstEndpoint.address)
-
         srcStaking = await OFTStakingMock.deploy(proxyOFT.address)
         dstStaking = await OFTStakingMock.deploy(dstOFT.address)
-
         // internal bookkeeping for endpoints (not part of a real deploy, just for this test)
         srcEndpoint.setDestLzEndpoint(dstOFT.address, dstEndpoint.address)
         dstEndpoint.setDestLzEndpoint(proxyOFT.address, srcEndpoint.address)
-
         // set each contracts source address so it can send to each other
         dstPath = ethers.utils.solidityPack(["address", "address"], [dstOFT.address, proxyOFT.address])
         srcPath = ethers.utils.solidityPack(["address", "address"], [proxyOFT.address, dstOFT.address])
         await proxyOFT.setTrustedRemote(dstChainId, dstPath) // for A, set B
         await dstOFT.setTrustedRemote(srcChainId, srcPath) // for B, set A
-
         // set each contracts source address so it can send to each other
         dstStakingAddressBytes32 = ethers.utils.defaultAbiCoder.encode(["address"], [dstStaking.address])
         srcStakingAddressBytes32 = ethers.utils.defaultAbiCoder.encode(["address"], [srcStaking.address])
         await srcStaking.setRemoteStakingContract(dstChainId, dstStakingAddressBytes32)
         await dstStaking.setRemoteStakingContract(srcChainId, srcStakingAddressBytes32)
-
         //set destination min gas
         await proxyOFT.setMinDstGas(dstChainId, parseInt(await proxyOFT.PT_SEND()), 225000)
         await proxyOFT.setUseCustomAdapterParams(true)
-
         owner = (await ethers.getSigners())[0]
         alice = (await ethers.getSigners())[1]
         bob = (await ethers.getSigners())[2]
         carol = (await ethers.getSigners())[3]
+    })
+
+    it("Should deploy the contract", async function () {
+        // const owner = await ethers.getSigner()
+
+        const Cert = await ethers.getContractFactory("Cert")
+        cert = await Cert.deploy()
+
+        expect(cert.deployTransaction.from).to.equal(owner.address)
+    })
+
+    it("Should issue the certificate", async function () {
+        console.log(cert)
+        await expect(cert.issue(1024, "Lindsey", "TTE", "S", "2186-06-12", "0x2f44454d59535449462f6e6578742d63657274696669636174652d646170702f"))
+            .to.emit(cert, "Issued")
+            .withArgs(1024, "TTE", "2186-06-12")
     })
 
     it("deposit on dst chain", async function () {
